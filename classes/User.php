169d4b81b008c2dc7049c842bfdabd73577ea962
Charlie280809 <vanbellelore@gmail.com>
2024-10-23 12:42:04 +0200

    feat: user-class base made

--- classes/User.php
+++ classes/User.php
@@ -1,10 +1,69 @@
 <?php
+    include_once(__DIR__."/Db.php");
     class User{
-        private $ID;
-        private $username;
+        private $id; //automatisch gegenereerd
+        private $username; //deze word ingevuld door de gebruiker zelf in de signup
+        private $email; //deze word ingevuld door de gebruiker zelf in de signup
+        private $password; //deze word ingevuld door de gebruiker zelf in de signup
+        private $is_admin; //deze word ingevuld door de gebruiker zelf in de signup
+        private $currency_balance;
+        private $created_at;
+        private $updated_at;
+        private $active; //standaard op 1 (als de gebruiker zich afmeld/het account verwijderd, dan wordt deze 0)
+        private $street_number;
+        private $street_name;
+        private $postal_code;
+        private $country;
 
-        public static function save (){
-            
+
+        public function getUsername(){
+            return $this->username;
         }
+        public function setUsername($username){
+            if(empty($username)){
+                throw new Exception("Please fill in a username.");
+            }
+            $this->username = $username;
+            return $this;
+        }
+        public function getEmail(){
+            return $this->email;
+        }
+        public function setEmail($email){
+            if(empty($email)){
+                throw new Exception("Please fill in your email.");
+            }
+            $this->email = $email;
+            return $this;
+        }
+
+        public function getPassword(){
+            return $this->password;
+        }
+        public function setPassword($password){
+            if(empty($password)){
+                throw new Exception("Please fill in a password.");
+            }
+
+            $options = [
+                'cost' => 15, 
+            ];
+            $hash = password_hash($password, PASSWORD_DEFAULT, $options); 
+
+            $this->password = $hash;
+            return $this;
+        }
+
+        
+        public function save (){
+            $conn = Db::getConnection();
+
+            $statement = $conn->prepare("INSERT INTO users(username, email, password) VALUES (:username, :email, :password);"); //accounts toevoegen in de databank
+            $statement->bindValue(":username", $this->getUsername());
+            $statement->bindValue(":email", $this->getEmail()); 
+            $statement->bindValue(":password", $this->getPassword());
+            $statement->execute();
+        }
+
     }
 ?>
