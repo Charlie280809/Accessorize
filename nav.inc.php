e56085bf81f075986bdf4ccd2cb1fe4343ee9124
Charlie280809 <vanbellelore@gmail.com>
2024-10-30 17:47:45 +0100

    feat: navigation made

---
+++ nav.inc.php
@@ -0,0 +1,10 @@
+<nav class="navbar">
+    <a href="index.php" class="logo">LOGO</a>
+    <a href="index.php">Home</a>
+    
+    <form action="" method="get">
+      <input type="text" name="search">
+    </form>
+    
+    <a href="logout.php" class="navbar__logout">Hi <?php echo htmlspecialchars($_SESSION['email']); ?>, logout?</a>
+</nav>
