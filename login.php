542751799aa48b0f65d643e310316883299140b0
Charlie280809 <vanbellelore@gmail.com>
2024-10-30 20:08:58 +0100

    feat: progress

--- login.php
+++ login.php
@@ -1,29 +1,40 @@
 <?php 
+    include_once(__DIR__."/classes/Db.php");   
     function canLogin($p_email, $p_password){
-        $conn = new PDO('mysql:host=localhost;dbname=accessorize', 'root', 'root');
+        $conn = Db::getConnection();
         $statement = $conn->prepare("SELECT * FROM `users` WHERE `email` = :email"); //preparen zodat men niet kan sjoemelen met die ':email'
 		$statement->bindValue(':email', $p_email); //':email' binden aan $p_email
         $statement->execute();
 
-        // $user = $statement->fetch(PDO::FETCH_ASSOC); //user linken met de databank
-        // if($user){
-		// 	$hash = $user['password']; //hash van user is password uit de databank
+        $user = $statement->fetch(PDO::FETCH_ASSOC); //user linken met de databank
+        if($user){
+			$hash = $user['password']; //hash van user is password uit de databank
 
-		// 	if(password_verify($p_password, $hash)){
-		// 		return true;
-		// 	}else{
-		// 		return false;
-		// 	}
-		// }else{
-		// 	//not found
-		// 	return false;
-		// }
-
-
+			if(password_verify($p_password, $hash)){
+				return true;
+			}else{
+				return false;
+			}
+		}else{
+			//not found
+			return false;
+		}
 
         //if(User::canLogin($email, $password)){  GO, maak sessie + redirect naar home  }
     }   
 
+    if(!empty($_POST)){
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        if(canLogin($email, $password)){
+            session_start();
+            $_SESSION['loggedin'] = true;
+            header('Location: index.php');
+        }else{
+            $error = true;
+        }
+    }
 
 ?><!DOCTYPE html>
 <html lang="en">
@@ -37,13 +48,13 @@
 <div class="form form_login">
         <form action="" method="post">
             
-            <!-- <//?php if(isset($error)): ?> als de variabele 'error' bestaat, voer code block uit
+            <?php if(isset($error)): ?> 
                 <div class="error">
                     <p>
                         Hmm, your e-mail or password must be wrong. Please try again.
                     </p>
                 </div>
-            <//?php endif; ?> -->
+            <?php endif; ?>
 
             <div class="form_field">					
                 <label for="Email">E-mail</label>
