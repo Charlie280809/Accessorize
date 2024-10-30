2914091d891f05cb897504ddacc8327b437fcd6b
Charlie280809 <vanbellelore@gmail.com>
2024-10-30 17:48:34 +0100

    feat: progress

--- classes/User.php
+++ classes/User.php
@@ -9,7 +9,7 @@
         private $currency_balance;
         private $created_at;
         private $updated_at;
-        private $active; //standaard op 1 (als de gebruiker zich afmeld/het account verwijderd, dan wordt deze 0)
+        private $active = 1; //standaard op 1 (als de gebruiker zich afmeld/het account verwijderd, dan wordt deze 0)
         private $street_number;
         private $street_name;
         private $postal_code;
@@ -46,7 +46,7 @@
             }
 
             $options = [
-                'cost' => 15, 
+                'cost' => 10, 
             ];
             $hash = password_hash($password, PASSWORD_DEFAULT, $options); 
 
@@ -55,15 +55,31 @@
         }
 
         
-        public function save (){
-            $conn = Db::getConnection();
+    //     public function save (){         
+    //         $conn = Db::getConnection();
 
-            $statement = $conn->prepare("INSERT INTO users(username, email, password) VALUES (:username, :email, :password);"); //accounts toevoegen in de databank
-            $statement->bindValue(":username", $this->getUsername());
-            $statement->bindValue(":email", $this->getEmail()); 
-            $statement->bindValue(":password", $this->getPassword());
-            $statement->execute();
+    //         $statement = $conn->prepare("INSERT INTO users(username, email, password) VALUES (:username, :email, :password);"); //accounts toevoegen in de databank
+    //         $statement->bindValue(":username", $this->getUsername());
+    //         $statement->bindValue(":email", $this->getEmail()); 
+    //         $statement->bindValue(":password", $this->getPassword());
+    //         return $statement->execute();
+    //     }
+
+    // }
+
+
+    public function save (){         
+        $conn = Db::getConnection();
+    
+        $statement = $conn->prepare("INSERT INTO users(username, email, password) VALUES (:username, :email, :password);");
+        $statement->bindValue(":username", $this->getUsername());
+        $statement->bindValue(":email", $this->getEmail()); 
+        $statement->bindValue(":password", $this->getPassword());
+    
+        if (!$statement->execute()) {
+            throw new Exception("Failed to save user.");
         }
-
+        return true;
     }
+}
 ?>
