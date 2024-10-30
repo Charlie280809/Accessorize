9d0318b5f9a4f98aacb4062b91d015ff2b384812
Charlie280809 <vanbellelore@gmail.com>
2024-10-30 20:07:59 +0100

    feat: progress

--- classes/Product.php
+++ classes/Product.php
@@ -1,4 +1,24 @@
 <?php
+    include_once("Db.php");
+
     //::getAll --> om alle producten op de homepage te kunnen tonen
     //save();
+
+class Product{
+    public function save($title, $price){
+        $conn = Db::getConnection();
+        $statement = $conn->prepare('INSERT INTO products (title, price) VALUES (:title, :price)');
+        $statement->bindParam(':title', $title);
+        $statement->bindParam(':price', $price);
+        $statement->execute();
+    }
+
+    public static function getAll(){
+        $conn = Db::getConnection();
+        $statement = $conn->prepare('SELECT * FROM products');
+        $statement->execute();
+        $products = $statement->fetchALL(PDO::FETCH_ASSOC);
+        return $products;
+    }
+} 
 ?>
