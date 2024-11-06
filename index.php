925a4a6251125261f92860b581aaa255ad877c56
Charlie280809 <vanbellelore@gmail.com>
2024-11-06 09:32:58 +0100

    feat: stylesheet linked

--- index.php
+++ index.php
@@ -12,13 +12,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accessorize home</title>
+    <link rel="stylesheet" href="css/style_index.css">
 </head>
 <body>
   <div class="accessorize">
     <?php include_once("nav.inc.php") ?>
     <h1>Accessorize</h1>
 
-        <?php foreach($products as $p): ?>
+        <?php foreach($products as $key => $p): ?>
+          <!-- <a href="product.php?id=<?php //echo $key ?>"></a> -->
         <article>
             <h2><?php echo $p['title'] ?>: €<?php echo $p['price'] ?></h2>
         </article>
