1609c0912f3ae83b56d358d3a518250e70792208
Charlie280809 <vanbellelore@gmail.com>
2024-11-09 23:52:40 +0100

    fix: move searchbar

--- nav.inc.php
+++ nav.inc.php
@@ -5,17 +5,14 @@
     <a href="logout.php" class="navbar__logout">Hey <?php echo htmlspecialchars($_SESSION['email']); ?>, logout?</a>
   </div>
 
-  <div class="navbar_search">
-    <form action="" method="get">
-      <input type="text" name="search" placeholder="Looking for something?">
-    </form>
-  </div>
-
   <div class="navbar_bottom">
     <a href="index.php" class="navbar__link">Home</a>
     <a href="#" class="navbar__link">Earrings</a>
     <a href="#" class="navbar__link">Rings</a>
     <a href="#" class="navbar__link">Necklaces</a>
     <a href="#" class="navbar__link">Bracelets</a>
+    <form class="search" action="" method="get">
+      <input type="text" name="search" placeholder="Looking for something?">
+    </form>
   </div>
 </nav>
