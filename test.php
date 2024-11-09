c554348975d673314569643c5d21606c4573331d
Charlie280809 <vanbellelore@gmail.com>
2024-11-09 11:12:33 +0100

    feat: more testing

--- test.php
+++ test.php
@@ -1,80 +1,57 @@
-<?php 
-    include_once(__DIR__."/classes/Db.php");   
-    function canLogin($p_email, $p_password){
-        $conn = Db::getConnection();
-        $statement = $conn->prepare("SELECT * FROM `users` WHERE `email` = :email"); //preparen zodat men niet kan sjoemelen met die ':email'
-		$statement->bindValue(":email", $p_email); //':email' binden aan $p_email
-        $statement->execute();
-
-        $user = $statement->fetch(PDO::FETCH_ASSOC); //user linken met de databank
-        if($user){ //als de user gevonden is in de databank 
-			$hash = $user['password']; //hash van user is password uit de databank
-
-			if(password_verify($p_password, $hash)){
-				return true;
-			}else{
-				return false;
-			}
-		}else{
-			//not found
-			return false;
-		}
-
-        //if(User::canLogin($email, $password)){  GO, maak sessie + redirect naar home  }
-    }   
-
-    if(!empty($_POST)){
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-
-        if(canLogin($email, $password)){
-            session_start();
-            $_SESSION['loggedin'] = true;
-            header('Location: index.php');
-            echo "logged in.";
-        }else{
-            $error = true;
-        }
-    }
-
+<?php
+  include_once(__DIR__."/classes/Product.php"); 
+  session_start();
+  if($_SESSION['loggedin']!== true){ //als 
+    header('Location: login.php');
+  }
+  else{
+    $products = Product::getAll();
+  }
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login to Accessorize</title>
-    <link rel="stylesheet" href="css/style_signup-login.css">
+    <title>Accessorize home</title>
+    <link rel="stylesheet" href="css/style_index.css">
 </head>
 <body>
-<div class="form_field">
-        <form action="" method="post">
+  <div class="accessorize">
+  <nav class="navbar">
+  <div class="navbar_top">
+    <a href="index.php" class="logo"><img src="Moon_Logo.png" alt="logo"></a>
+    <h2>Accessorize</h2>
+    <a href="logout.php" class="navbar__logout">Hey <?php echo htmlspecialchars($_SESSION['email']); ?>, logout?</a>
+  </div>
 
-            <h2>Login to Accessorize</h2>
-            
-            <?php if(isset($error)): ?> 
-                <div class="error">
-                    <p>
-                        Hmm, your e-mail or password must be wrong. Please try again.
-                    </p>
-                </div>
-            <?php endif; ?>
+  <div class="navbar_search">
+    <form action="" method="get">
+      <input type="text" name="search" placeholder="Looking for something?">
+    </form>
+  </div>
 
-            <div>					
-                <label for="Email">E-mail</label>
-				<input type="text" name="email">
-			</div>
+  <div class="navbar_bottom">
+    <a href="index.php" class="navbar__link">Home</a>
+    <a href="#" class="navbar__link">Earrings</a>
+    <a href="#" class="navbar__link">Rings</a>
+    <a href="#" class="navbar__link">Necklaces</a>
+    <a href="#" class="navbar__link">Bracelets</a>
+  </div>
+</nav>
 
-			<div>
-				<label for="Password">Password</label>
-				<input type="password" name="password">
-			</div>
-
-			<div>
-				<input type="submit" value="Sign in" class="btn_submit">	
-				<!-- <input type="checkbox" id="rememberMe"><label for="rememberMe" class="label__inline">Remember me</label> -->
-			</div>
-
-        </form>
+    <div class="collection">
+      <?php foreach($products as $key => $p): ?>
+        <a href="product.php?id=<?php echo $key ?>">
+          <div class="collection_product">
+            <img src="Moon_Logo.png" alt="<?php echo $p['title'] ?>">
+            <div class="product_details">
+              <p class="product_title"><?php echo $p['title'] ?></p>
+              <p class="product_price">€<?php echo $p['price'] ?></p>
+            </div>
+          </div>
+        </a>
+      <?php endforeach; ?>
     </div>
+  </div>
 </body>
 </html>
