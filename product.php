f189c72bf6db35de3fcd6daf5ec8f654f634f517
Charlie280809 <vanbellelore@gmail.com>
2024-11-09 11:13:25 +0100

    feat: add stylesheet

--- product.php
+++ product.php
@@ -5,7 +5,7 @@
 <head>
   <meta charset="UTF-8">
   <title>Product_details</title>
-  <!-- <link rel="stylesheet" href=""> -->
+  <link rel="stylesheet" href="css/style_index.css">
 </head>
 <body>
   
@@ -13,11 +13,11 @@
   <?php include_once("nav.inc.php"); ?>
   
   <!-- <div class="collectionDetails" style="background-image: linear-gradient(to right, rgba(0,0,0,1) 0%,rgba(255,255,255,0) 100%), url(<?php echo $content['poster'] ?>)"> -->
-    <!-- <h1 class="collectionDetails__title"><?php echo $content['title'] ?></h1> -->
-    <p class="collectionDetails__desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+    <!-- <h1 class="collectionDetails__title"><?php //echo $content['title'] ?></h1> -->
+    <p class="">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
   
     <!-- <form action="" method="post">
-    <input type="hidden" name="collectionId" value="<?php echo $id ?>">
+    <input type="hidden" name="collectionId" value="<?php //echo $id ?>">
     <input class="btn btn--primary" name="btnAdd" type="submit" value="Add to list">
     </form> -->
 
