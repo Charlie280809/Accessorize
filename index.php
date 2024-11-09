615f3400d9f0485e5e2710beab13aad46f646ce9
Charlie280809 <vanbellelore@gmail.com>
2024-11-09 11:11:57 +0100

    feat: product-template base made

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
   include_once(__DIR__."/classes/Product.php"); 
   session_start();
-  if($_SESSION['loggedin']!== true){
+  if($_SESSION['loggedin']!== true){ //als 
     header('Location: login.php');
-  }else{
+  }
+  else{
     $products = Product::getAll();
   }
 ?><!DOCTYPE html>
@@ -17,15 +18,18 @@
 <body>
   <div class="accessorize">
     <?php include_once("nav.inc.php") ?>
-    <h1>Accessorize</h1>
 
     <div class="collection">
       <?php foreach($products as $key => $p): ?>
-          <a href="product.php?id=<?php echo $key ?>">linkk voor <?php echo $key?></a>
-
-        <article>
-            <h2><?php echo $p['title'] ?>: €<?php echo $p['price'] ?></h2>
-        </article>
+        <a href="product.php?id=<?php echo $key ?>">
+          <div class="collection_product">
+            <img src="Moon_Logo.png" alt="<?php echo $p['title'] ?>">
+            <div class="product_details">
+              <p class="product_title"><?php echo $p['title'] ?></p>
+              <p class="product_price">€<?php echo $p['price'] ?></p>
+            </div>
+          </div>
+        </a>
       <?php endforeach; ?>
     </div>
   </div>
