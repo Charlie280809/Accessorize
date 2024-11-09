77a5270b24893f02a7990c06f49100f6a54fc760
Charlie280809 <vanbellelore@gmail.com>
2024-11-09 11:12:17 +0100

    feat: add categories

--- nav.inc.php
+++ nav.inc.php
@@ -1,15 +1,21 @@
 <nav class="navbar">
   <div class="navbar_top">
-    <a href="index.php" class="logo"><img src="moon_logo.pdf" alt="logo"></a>
+    <a href="index.php" class="logo"><img src="Moon_Logo.png" alt="logo"></a>
     <h2>Accessorize</h2>
     <a href="logout.php" class="navbar__logout">Hey <?php echo htmlspecialchars($_SESSION['email']); ?>, logout?</a>
   </div>
 
+  <div class="navbar_search">
+    <form action="" method="get">
+      <input type="text" name="search" placeholder="Looking for something?">
+    </form>
+  </div>
+
   <div class="navbar_bottom">
     <a href="index.php" class="navbar__link">Home</a>
-    <a href="product.php" class="navbar__link">Earrings</a>
-    <a href="cart.php" class="navbar__link">Rings</a>
-    <a href="cart.php" class="navbar__link">Necklaces</a>
-    <a href="cart.php" class="navbar__link">Bracelets</a>
+    <a href="#" class="navbar__link">Earrings</a>
+    <a href="#" class="navbar__link">Rings</a>
+    <a href="#" class="navbar__link">Necklaces</a>
+    <a href="#" class="navbar__link">Bracelets</a>
   </div>
 </nav>
