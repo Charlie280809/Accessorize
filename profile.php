f5e3f93af96b3673ce22bae66e7f06548bd085f3
Charlie280809 <vanbellelore@gmail.com>
2024-11-11 17:41:51 +0100

    feat: add new product template made

--- profile.php
+++ profile.php
@@ -11,11 +11,47 @@
 <body>
     <?php include_once("nav.inc.php") ?>
     <div class="profile">
-        <h2>Profile</h2>
-        <h4>Hello there, <?php echo htmlspecialchars($_SESSION['email']);?>. You are an admin, that means you can also create, update and delete your own products.</h4>
-        <p>testing testing testing...</p>
-
+        <!-- <h2>Profile</h2> -->
+        <h4>Hello there, <?php echo htmlspecialchars($_SESSION['email']);?>. You are an admin, which means you can create, update and delete your own products to the shop.</h4>
+        <form action="" method="post" class="new_product">
+            <div>					
+                <label for="Title">Product title</label>
+				<input type="text" name="title">
+			</div>
+            <div>					
+                <label for="Price">Product price</label>
+				<input type="text" name="price">
+			</div>
+            <div>					
+                <label for="Email">E-mail</label>
+                <!-- make dropdown -->
+				<input type="dropdown" name="email">
+                <option value="earrings">Earrings</option>
+                <option value="rings">Rings</option>
+                <option value="necklaces">Necklaces</option>
+                <option value="bracelets">Bracelets</option>
+			</div>
+            <div>					
+                <label for="Description">Product description</label>
+				<input type="text" name="description">
+			</div>
+            <div>					
+                <label for="Color">Product color</label>
+				<input type="text" name="color">
+			</div>
+            <div>					
+                <label for="Stock_amount">How many do you have in stock? ()</label>
+				<input type="text" name="stock_amount">
+			</div>
+            <div>
+                <input type="submit" value="Add new product to shop" class="addbtn">
+            </div>
+        </form>
+            <br>
+        <p>Change password (?)</p>
+            <br>
+        <p>Deactivate account (?)</p>
+        <a href="logout.php" class="logoutbtn">Log out?</a>
     </div>
-    <a href="logout.php" class="logoutbtn">Log out?</a>
 </body>
 </html>
