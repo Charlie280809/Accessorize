16bf73ccccbda031b33b00d690f595a740730f3b
Charlie280809 <vanbellelore@gmail.com>
2024-11-13 12:44:06 +0100

    feat: getter and setter fro admin

--- classes/Admin.php
+++ classes/Admin.php
@@ -3,11 +3,12 @@
     class Admin extends User{
         private $is_admin = 1;
 
-    //     public function getIs_admin(){
-    //         return $this->is_admin;
-    //      }
-    //      public function setIs_admin($is_admin){
-    //          $this->is_admin = ($is_admin === 'on') ? 1 : 0;
-    //      s}
+        public function getIs_admin(){
+                return $this->is_admin;
+        }
+        public function setIs_admin($is_admin){
+                $this->is_admin = $is_admin;
+                return $this;
+        }
     }
 ?>
