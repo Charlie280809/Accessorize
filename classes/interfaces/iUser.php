e503c81bf9cbdff137d68cfda22583f2daee2da5
Charlie280809 <vanbellelore@gmail.com>
2024-11-13 12:22:50 +0100

    feat: interfaceUser- template made

---
+++ classes/interfaces/iUser.php
@@ -0,0 +1,8 @@
+<?php
+    namespace App\Accessorize\Interfaces;
+    interface iUser{
+        public function login();
+        public function canLogin($p_email, $p_password);
+        public function logout();
+    }
+?>
