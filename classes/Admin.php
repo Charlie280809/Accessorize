0a9d3dea5550c5f5b20dcac9c1b7f65c3d022eb6
Charlie280809 <vanbellelore@gmail.com>
2024-11-13 12:22:07 +0100

    feat: subclasses admin and customer made

---
+++ classes/Admin.php
@@ -0,0 +1,13 @@
+<?php
+    namespace App\Accessorize;
+    class Admin extends User{
+        private $is_admin = 1;
+
+    //     public function getIs_admin(){
+    //         return $this->is_admin;
+    //      }
+    //      public function setIs_admin($is_admin){
+    //          $this->is_admin = ($is_admin === 'on') ? 1 : 0;
+    //      s}
+    }
+?>
---
+++ classes/Customer.php
@@ -0,0 +1,6 @@
+<?php
+    namespace App\Accessorize;
+    class Customer extends User{
+        
+    }
+?>
