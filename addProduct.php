2c22cc02a7049bce2ca360a4aac9e6d5ef1a447e
Charlie280809 <vanbellelore@gmail.com>
2024-11-13 18:21:01 +0100

    fix: change page-name

--- addProduct.php
+++ addProduct.php
@@ -24,7 +24,7 @@ include_once(__DIR__."/classes/Product.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accessorize Profile</title>
+    <title>Add product</title>
     <link rel="stylesheet" href="css/style_index.css">
 </head>
 <body>
