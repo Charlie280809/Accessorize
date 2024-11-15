afefa96bf73c73d3bbf9a4453f0ae19d262d3e8d
Charlie280809 <vanbellelore@gmail.com>
2024-11-15 12:03:58 +0100

    feat: namespace added

--- classes/Product.php
+++ classes/Product.php
@@ -1,4 +1,5 @@
 <?php
+    namespace App\Accessorize;
     include_once("Db.php");
 class Product{
     private $id;
@@ -14,7 +15,7 @@ class Product{
     }
     public function setTitle($title){
         if(empty((string)$title)){
-            throw new Exception("Please fill in a title for your product.");
+            throw new \Exception("Please fill in a title for your product.");
         }
         $this->title = $title;
         return $this;
@@ -25,9 +26,9 @@ class Product{
     }
     public function setPrice($price){
         if(empty($price)){ //als de prijs leeg is
-            throw new Exception("Please fill in a price for your product.");
+            throw new \Exception("Please fill in a price for your product.");
         }else if($price <= 0.00){ //als de prijs gelijk of kleiner is dan 0,00
-            throw new Exception("Your price can't be negative!");
+            throw new \Exception("Your price can't be negative!");
         }
         $this->price = $price;
         return $this;
@@ -47,7 +48,7 @@ class Product{
     }
     public function setDescription($description){
         if(empty($description)){ //als de beschrijving leeg is
-            throw new Exception("Please fill in a description for you product.");
+            throw new \Exception("Please fill in a description for you product.");
         }
         $this->description = $description;
         return $this;
@@ -58,7 +59,7 @@ class Product{
     }
     public function setColor($color){
         if(empty($color)){
-            throw new Exception("Please fill in the color your product has.");
+            throw new \Exception("Please fill in the color your product has.");
         }
         $this->color = $color;
         return $this;
@@ -69,7 +70,7 @@ class Product{
     }
     public function setStock_amount($stock_amount){
         if(empty($stock_amount)){
-            throw new Exception("Please fill in the product's stock amount.");
+            throw new \Exception("Please fill in the product's stock amount.");
         }
         $this->stock_amount = $stock_amount;
         return $this;
@@ -88,10 +89,10 @@ class Product{
     }
 
     public static function getAll(){
-        $conn = Db::getConnection();
+        $conn =  Db::getConnection();
         $statement = $conn->prepare('SELECT * FROM products');
         $statement->execute();
-        $products = $statement->fetchALL(PDO::FETCH_ASSOC);
+        $products = $statement->fetchALL(\PDO::FETCH_ASSOC);
         return $products;
     }
 } 
