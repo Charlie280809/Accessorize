c92e1bf15ba39a95d154529949e5d1b87ea92a4a
Charlie280809 <vanbellelore@gmail.com>
2024-11-30 15:51:44 +0100

    test azure

--- classes/Db.php
+++ classes/Db.php
@@ -1,26 +1,26 @@
 <?php
     namespace App\Accessorize;
-    class Db{
-        private static $conn;
-        const SETTINGS = [
-            "user" => "accessorizeCharlieVBE",
-            "password" => "TvRgb4yURr.b-7B",
-            "host" => "accessorize.mysql.database.azure.com",
-            "db" => "accessorize",
-            "ssl_ca" => __DIR__ . "./DigiCertGlobalRootG2.crt.pem"
+    // class Db{
+    //     private static $conn;
+    //     const SETTINGS = [
+    //         "user" => "accessorizeCharlieVBE",
+    //         "password" => "TvRgb4yURr.b-7B",
+    //         "host" => "accessorize.mysql.database.azure.com",
+    //         "db" => "accessorize",
+    //         "ssl_ca" => __DIR__ . "./DigiCertGlobalRootG2.crt.pem"
 
-        ];
-        public static function getConnection(){
-            if(self::$conn == null){
-                $options[\PDO::MYSQL_ATTR_SSL_CA] = self::SETTINGS['ssl_ca'];
-                    self::$conn = new \PDO('mysql:host='.self::SETTINGS["host"].';dbname='.self::SETTINGS["db"].'',self::SETTINGS["user"],self::SETTINGS["password"], $options);
-                return self::$conn;
-            }
-            else{
-                return self::$conn;
-            }
-        }
-    }
+    //     ];
+    //     public static function getConnection(){
+    //         if(self::$conn == null){
+    //             $options[\PDO::MYSQL_ATTR_SSL_CA] = self::SETTINGS['ssl_ca'];
+    //                 self::$conn = new \PDO('mysql:host='.self::SETTINGS["host"].';dbname='.self::SETTINGS["db"].'',self::SETTINGS["user"],self::SETTINGS["password"], $options);
+    //             return self::$conn;
+    //         }
+    //         else{
+    //             return self::$conn;
+    //         }
+    //     }
+    // }
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
