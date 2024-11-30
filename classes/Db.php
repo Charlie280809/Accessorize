ce8db186ce299d89a0b37b2de1e94f708e252cf5
Charlie280809 <vanbellelore@gmail.com>
2024-11-30 15:46:06 +0100

    feat: trying to deploy, changes made in Db

--- classes/Db.php
+++ classes/Db.php
@@ -1,15 +1,22 @@
 <?php
     namespace App\Accessorize;
     class Db{
-        private static $conn = null;
+        private static $conn;
+        const SETTINGS = [
+            "user" => "accessorizeCharlieVBE",
+            "password" => "TvRgb4yURr.b-7B",
+            "host" => "accessorize.mysql.database.azure.com",
+            "db" => "accessorize",
+            "ssl_ca" => __DIR__ . "./DigiCertGlobalRootG2.crt.pem"
+
+        ];
         public static function getConnection(){
             if(self::$conn == null){
-                // echo "👾";
-                self::$conn = new \PDO('mysql:host=localhost;dbname=accessorize','root','root');
+                $options[\PDO::MYSQL_ATTR_SSL_CA] = self::SETTINGS['ssl_ca'];
+                    self::$conn = new \PDO('mysql:host='.self::SETTINGS["host"].';dbname='.self::SETTINGS["db"].'',self::SETTINGS["user"],self::SETTINGS["password"], $options);
                 return self::$conn;
             }
             else{
-                //echo "❗";
                 return self::$conn;
             }
         }
