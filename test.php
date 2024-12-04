f854f2c0d287edc1fa5b6755175b8ef4b19c3873
Charlie280809 <vanbellelore@gmail.com>
2024-12-04 12:16:25 +0100

    testing

--- test.php
+++ test.php
@@ -1,7 +1,9 @@
 <?php
     namespace App\Accessorize;
-    require_once __DIR__.'classes/Order.php';
+    require_once __DIR__.'/vendor/autoload.php';
     require_once __DIR__.'classes/Db.php';
+    require_once __DIR__.'classes/Order.php';
+    
     $conn = Db::getConnection();
     $statment = $conn->prepare("SELECT * FROM users");
     $statment->execute();
