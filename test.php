12adfb08ec4c523102ac3e4910219c3dc467844d
Charlie280809 <vanbellelore@gmail.com>
2024-12-04 10:18:06 +0100

    testing

--- test.php
+++ test.php
@@ -1,10 +1,12 @@
 <?php
-    namespace App\Accessorize;
-    //make database connection
-    include_once 'classes/Db.php';
-    $conn = Db::getConnection();
-    $statment = $conn->prepare("SELECT * FROM users");
-    $statment->execute();
+    echo 'hallo';
+    exit();
+    // namespace App\Accessorize;
+    // //make database connection
+    // include_once 'classes/Db.php';
+    // $conn = Db::getConnection();
+    // $statment = $conn->prepare("SELECT * FROM users");
+    // $statment->execute();
 
-    var_dump($statment->fetchAll());
+    // var_dump($statment->fetchAll());
 ?>
