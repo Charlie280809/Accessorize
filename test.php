80412a14e64bf91d16b119fce97dec5d80bc7fd3
Charlie280809 <vanbellelore@gmail.com>
2024-12-05 17:53:11 +0100

    feat: testing cart layout

--- test.php
+++ test.php
@@ -10,3 +10,34 @@
 
     var_dump($statment->fetchAll());
 ?>
+
+ <!-- <table border="1">
+            <thead>
+                <tr>
+                    <th>Product</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                    <th>Subtotal</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php // foreach ($_SESSION['cart'] as $item): ?>
+                    <tr>
+                        <td><?= //htmlspecialchars($item['name']); ?></td>
+                        <td>€<?= //number_format($item['price'], 2); ?></td>
+                        <td><?=  //$item['quantity']; ?></td>
+                        <td>€<?=  //number_format($item['price'] * $item['quantity'], 2); ?></td>
+                        <td>
+                            <a href="cart.php?action=remove&id=<?= // $item['id']; ?>">Remove</a>
+                        </td>
+                    </tr>
+                <?php // endforeach; ?>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td colspan="3">Total</td>
+                    <td>€<?= // number_format($totalPrice, 2); ?></td>
+                </tr>
+            </tfoot>
+        </table> -->
