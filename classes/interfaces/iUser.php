d53991715f7e3ef47ca4db3876a11590f049a4b4
Charlie280809 <vanbellelore@gmail.com>
2024-12-05 23:20:54 +0100

    fix: delete unused files

--- classes/interfaces/iUser.php
+++
@@ -1,8 +0,0 @@
-<?php
-    namespace App\Accessorize\Interfaces;
-    interface iUser{
-        // public function login();
-        public function canLogin($p_email, $p_password);
-        // public function logout();
-    }
-?>
