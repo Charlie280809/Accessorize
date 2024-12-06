03b55ba51caa8a3c7f8c0c50a9c63c3f8ac82bb5
Charlie280809 <vanbellelore@gmail.com>
2024-12-06 11:31:03 +0100

    fix: remove changePassword-logic from page

--- userInfo.php
+++ userInfo.php
@@ -1,4 +1,5 @@
 <?php
+    namespace App\Accessorize;
     include_once(__DIR__."/classes/Db.php");
     include_once(__DIR__."/classes/User.php"); 
     session_start();
@@ -7,72 +8,23 @@
         header('Location: login.php');
     }
     else{
-        $user = App\Accessorize\User::getUserByEmail($_SESSION['email']); //get user by email
-        
-        if(!empty($_POST)){ //if the POST is not empty
-            $currentPassword = $_POST['current_password'];
-            $newPassword = $_POST['new_password'];
-            $confirmPassword = $_POST['confirm_password'];
-
-            if(empty($currentPassword) || empty($newPassword) || empty($confirmPassword)) {
-                $error = "Please fill in all fields."; //check if fields are empty
-            }elseif($newPassword !== $confirmPassword) {
-                $error = "Your new passwords do not match."; //check if new passwords match
-            }else{
-                if (!password_verify($currentPassword, $user['password'])) {
-                    $error = "Your current password is incorrect."; //check if current password is correct
-                } else {
-                    $options = ['cost' => 12];
-                    $hash = password_hash($newPassword, PASSWORD_DEFAULT, $options);
-        
-                    App\Accessorize\User::changePassword($_SESSION['email'], $hash);
-                    $succes = "New password saved!";
-                }
-            }
-        }        
+        $currentUser = User::getUserByEmail($_SESSION['email']);
+        //
     }
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $user['username']. ' details'?></title>
+    <title><?php echo $currentUser['username']. ' details'?></title>
     <link rel="stylesheet" href="css/style_index.css">
 </head>
 <body>
     <?php include_once("nav.inc.php") ?>
     <div class="profile">
-        
-        <div class="change_password">
-            <form action="" method="post">
-            <h3>Change password?</h3>
-            
-                <?php if(isset($error)): ?> 
-                    <p class="error"><?php echo $error; ?></p>
-                <?php endif; ?>
-    
-            <div><?php echo $succes ?></div>
-
-            <div>					
-                <label for="Oldpassword">Enter your current password</label>
-				<input type="text" name="current_password">
-							
-                <label for="NewPassword">Enter your new password</label>
-				<input type="text" name="new_password">
-
-                <label for="confirmPassword">Enter your new password</label>
-				<input type="text" name="confirm_password">
-			
-                <input type="submit" value="Change my password" class="changebtn">
-            </div>
-        </div>
-
-            
-        </form>
-            <br>
-        <!-- <p>Change password (?)</p> -->
-            <br>
-        <!-- <p>Deactivate account (?)</p> -->
+        <a href="changePassword.php">Change password</a>
+        <br>
+        <p>Deactivate account (?)</p>
         <a href="logout.php" class="logoutbtn">Log out?</a>
     </div>
 </body>
