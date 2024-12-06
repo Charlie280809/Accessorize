6c1a9c7ab9628a523e8f50ad4fc62de07256c3ee
Charlie280809 <vanbellelore@gmail.com>
2024-12-06 11:10:03 +0100

    fix: correct userId with review

--- ajax/savereview.php
+++ ajax/savereview.php
@@ -1,32 +1,31 @@
 <?php
 namespace App\Accessorize;
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
+include_once(__DIR__."/../classes/Db.php");
 include_once(__DIR__."/../classes/Review.php");
+include_once(__DIR__."/../classes/User.php");
+session_start();
 
 if(!empty($_POST)){
 
-    error_log("POST data ontvangen: " . print_r($_POST, true)); // Log naar PHP-log
+    $currentUser = User::getUserByEmail($_SESSION['email']); //get 
+    $userId = $currentUser['id'];
 
+    $r = new Review();
+    $r->setUserId($userId);
+    $r->setProductId($_POST['productId']);
+    $r->setContent($_POST['content']);
 
-        $r = new Review();
-        $r->setUserId(1); //$_SESSION
-        $r->setProductId($_POST['productId']);
-        $r->setContent($_POST['content']);
+    $r->save();
 
-        $r->save();
+    $response = [
+        'status' => 'success',
+        'body' => htmlspecialchars($r->getContent()),
+        'message' => 'Review saved'
+    ];
 
-        $response = [
-            'status' => 'success',
-            'body' => htmlspecialchars($r->getContent()),
-            'message' => 'Review saved'
-        ];
-
-        header('Content-Type: application/json');
-        echo json_encode($response); //json_encode zet array om naar json
+    header('Content-Type: application/json');
+    echo json_encode($response); //json_encode zet array om naar json
 } else {
     error_log("Lege POST data");
     header('Content-Type: application/json');
