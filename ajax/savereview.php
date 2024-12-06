2904f7e523436f871a43f4e37eb1fd600791047b
Charlie280809 <vanbellelore@gmail.com>
2024-12-06 16:06:49 +0100

    fix: correct code

--- ajax/savereview.php
+++ ajax/savereview.php
@@ -1,35 +1,40 @@
 <?php
-namespace App\Accessorize;
+    namespace App\Accessorize;
 
-include_once(__DIR__."/../classes/Db.php");
-include_once(__DIR__."/../classes/Review.php");
-include_once(__DIR__."/../classes/User.php");
-session_start();
+    include_once(__DIR__."/../classes/Db.php");
+    include_once(__DIR__."/../classes/Review.php");
+    include_once(__DIR__."/../classes/User.php");
+    session_start();
 
-if(!empty($_POST)){
+    if(!empty($_POST)) {
+        try {
+            $currentUser = User::getUserByEmail($_SESSION['email']);
+            $userId = $currentUser['id'];
 
-    $currentUser = User::getUserByEmail($_SESSION['email']); //get 
-    $userId = $currentUser['id'];
+            $r = new Review();
+            $r->setUserId($userId);
+            $r->setProductId($_POST['productId']);
+            $r->setContent($_POST['content']);
 
-    $r = new Review();
-    $r->setUserId($userId);
-    $r->setProductId($_POST['productId']);
-    $r->setContent($_POST['content']);
+            $r->save();
 
-    $r->save();
+            $response = [
+                'status' => 'success',
+                'body' => htmlspecialchars($r->getContent()),
+                'message' => 'Review saved'
+            ];
+        } catch (\Exception $e) {
+            $response = [
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ];
+        }
 
-    $response = [
-        'status' => 'success',
-        'body' => htmlspecialchars($r->getContent()),
-        'message' => 'Review saved'
-    ];
-
-    header('Content-Type: application/json');
-    echo json_encode($response); //json_encode zet array om naar json
-} else {
-    error_log("Lege POST data");
-    header('Content-Type: application/json');
-    echo json_encode(['status' => 'error', 'message' => 'No data received']);
-    exit;
-}
+        header('Content-Type: application/json');
+        echo json_encode($response); // JSON response terugsturen
+    } else {
+        header('Content-Type: application/json');
+        echo json_encode(['status' => 'error', 'message' => 'No data received']);
+        exit;
+    }
 ?>
