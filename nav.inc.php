7a02a78aa52dbbf2c125915b27d9adce712cd677
Charlie280809 <vanbellelore@gmail.com>
2024-12-07 17:23:31 +0100

    fix: get currency_balance by previously declared variable

--- nav.inc.php
+++ nav.inc.php
@@ -2,7 +2,7 @@
   namespace App\Accessorize;
   require_once __DIR__.'./bootstrap.php';
   use App\Accessorize\User;
-    $currentUser = User::getUserByEmail($_SESSION['email']);
+  $currentUser = User::getUserByEmail($_SESSION['email']);
 ?><nav class="navbar">
   <div class="navbar_top">
     <a href="index.php" class="logo"><img src="Moon_Logo.png" alt="logo"></a>
@@ -12,7 +12,7 @@
         <a href="userInfo.php" class="navbar__logout">Profile <?php echo $currentUser['username']; ?></a>
       </div>
       <?php if($_SESSION['role'] == 0): //als de gebruiker geen admin is ?> 
-        <p><?php echo 'Your balance: €'.htmlspecialchars($_SESSION['currency_balance']); ?></p>
+        <p><?php echo 'Your balance: €'.htmlspecialchars($currentUser['currency_balance']); ?></p>
       <?php endif; ?>
       <div>
         <a href="cart.php" class="navbar__logout">Cart🛒</a>
