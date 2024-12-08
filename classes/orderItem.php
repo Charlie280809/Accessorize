4b0cedae217458cf0abba9a8b0927f6887a15bd2
Charlie280809 <vanbellelore@gmail.com>
2024-12-08 16:59:58 +0100

    fix: remove little test

--- classes/orderItem.php
+++ classes/orderItem.php
@@ -2,7 +2,6 @@
     namespace App\Accessorize;
     require_once __DIR__."/Db.php";
     
-    echo 'orderitem callss conected';
     class orderItem {
         private $id;
         private $order_id;
