dada710d7eb4f286bdca411adef081e990eda941
Charlie280809 <vanbellelore@gmail.com>
2024-12-08 19:46:04 +0100

    fix: update product + style page

--- updateProduct.php
+++ updateProduct.php
@@ -41,11 +41,11 @@
             }
         }
 
-        // if(isset($_POST['delete_product'])){
-        //     $product::deleteById($product['id']);
-        //     echo 'product deleted';
-        //     header('Location: index.php');
-        // }
+        if(isset($_POST['delete_product'])){
+            $product::deleteById($product['id']);
+            $error = 'product deleted';
+            header('Location: index.php');
+        }
     }else{ //if the user is not an admin
         header('Location: index.php');
     }
@@ -55,38 +55,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>update product</title>
+    <link rel="stylesheet" href="css/style_index.css">
 </head>
 <body>
-    <h2>Update product</h2>
-    <?php if(isset($error)): ?>
-        <p><?php echo $error; ?></p>
-    <?php endif; ?>
+    <?php include_once("nav.inc.php") ?>
+    <div class="updateProductPage">
+        <h2>Update product</h2>
+        <?php if(isset($error)): ?>
+            <p><?php echo $error; ?></p>
+        <?php endif; ?>
 
-    <?php if(isset($succes)): ?>
-        <p><?php echo $succes; ?></p>
-    <?php endif; ?>
+        <?php if(isset($succes)): ?>
+            <p><?php echo $succes; ?></p>
+        <?php endif; ?>
 
-    <form method="POST">
+        <form class="update_form" method="POST">
+            <div>
+                <label for="Title">Product title</label>
+                <input type="text" name="title" value="<?php echo htmlspecialchars($product['title']) ?>">
+            </div>
+            <div>
+                <label for="Price">Product price</label>
+                <input type="text" step="0.01" name="price" value="<?php echo htmlspecialchars($product['price']) ?>">
+            </div>
+            <div>
+                <label for="Description">Product description</label>
+                <input type="text" name="description" value="<?php echo htmlspecialchars($product['description']) ?>">
+            </div>
+            <div>
+                <label for="Stock">Stock amount</label>
+                <input type="text" name="stock_amount" value="<?php echo htmlspecialchars($product['stock_amount']) ?>">
+            </div>
 
-        <div>
-            <label for="Title">Product title</label>
-            <input type="text" name="title" value="<?php echo htmlspecialchars($product['title']) ?>">
-        </div>
-        <div>
-            <label for="Price">Product price</label>
-            <input type="text" step="0.01" name="price" value="<?php echo htmlspecialchars($product['price']) ?>">
-        </div>
-        <div>
-            <label for="Description">Product description</label>
-            <input type="text" name="description" value="<?php echo htmlspecialchars($product['description']) ?>">
-        </div>
-        <div>
-            <label for="Stock">Stock amount</label>
-            <input type="text" name="stock_amount" value="<?php echo htmlspecialchars($product['stock_amount']) ?>">
-        </div>
-
-        <button type="submit" name="update_product">Update Product</button>
-        <button type="submit" name="delete_product">Delete Product</button>
-    </form>
+            <button class="updateProductbtn" type="submit" name="update_product">Update Product</button>
+            <button class="deleteProductbtn" type="submit" name="delete_product">Delete Product</button>
+        </form>
+    </div>
 </body>
 </html>
