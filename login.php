a26cd130538852e69bf32dbfb4b80fec5ecf8b23
Charlie280809 <vanbellelore@gmail.com>
2024-12-08 15:31:13 +0100

    fix: enable errors

--- login.php
+++ login.php
@@ -1,5 +1,9 @@
 <?php
     namespace App\Accessorize;
+
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+
     require_once __DIR__.'./classes/Db.php';
     require_once __DIR__.'./classes/User.php';
 
